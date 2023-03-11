b427e1c74ea14c9cf5f33e618bfcc57183f0dc03
Karthik <110587940+karthikkalla08@users.noreply.github.com>
2023-03-11 15:15:52 +0530

    Add files via upload

---
+++ text to speech.py
@@ -0,0 +1,8 @@
+from gtts import gTTS
+import os
+abc = open('ob.txt')
+text = abc.read()
+language = 'en'
+obj = gTTS(text = text,lang =language,slow =False)
+obj.save("sample.mp3")
+os.system("sample.mp3")
